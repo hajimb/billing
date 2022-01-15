9c6dd5fa17c9d667a617a3cc49fc29b0c7dd1e83
mufaddal <haji@badricomputers.co.in>
2022-01-15 11:57:53 +0530

    checklogin

--- application/hooks/CheckLogin.php
+++ application/hooks/CheckLogin.php
@@ -8,12 +8,11 @@ class CheckLogin
         $class_name         = $ci->router->fetch_class();
 		// echo "ClassName:[$class_name]";
         $session_data       = $ci->session->userdata('user_session');
-
-        $group_data         = getUserGroupByUserId($session_data['user_id']);
-        $user_permission    = explode(',',$group_data['permission']);
-        $ci->session->set_userdata('user_permission', $user_permission);
-
+            
         if(strtolower($class_name) != "dashboard" && strtolower($class_name) != 'login' && strtolower($class_name) != 'order' ){
+            $group_data         = getUserGroupByUserId($session_data['user_id']);
+            $user_permission    = explode(',',$group_data['permission']);
+            $ci->session->set_userdata('user_permission', $user_permission);
             if (!isset($session_data) || empty($session_data)) {
                 redirect('login');
             }else{
